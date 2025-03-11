944aeb0d2f914a8517d45f4787cfd12b6ee497df
Yusuf - يُوسُفُ <zakiyousef15@gmail.com>
2025-03-11 08:34:21 +0200

    fix package

--- dist/src/components/Project.d.ts
+++ dist/src/components/Project.d.ts
@@ -1,7 +1,8 @@
-declare const Project: ({ title, description, en }: {
+declare const Project: ({ title, description, en, link }: {
     title: any;
     description: any;
     en: any;
+    link: any;
 }) => import("react/jsx-runtime").JSX.Element;
 export default Project;
 //# sourceMappingURL=Project.d.ts.map
--- dist/src/components/Project.js
+++ dist/src/components/Project.js
@@ -1,6 +1,6 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import Link from "next/link";
-const Project = ({ title, description, en }) => {
-    return (_jsxs("div", { className: "flex flex-col items-center overflow-hidden  ", children: [_jsx(Link, { href: `https://${en}.mafazaa.com`, className: "w-full", children: _jsx("div", { className: "w-full h-56 md:h-80 bg-black flex items-center justify-center rounded-lg", children: _jsx("h2", { className: "text-bright-one font-bold text-2xl md:text-3xl lg:text-5xl ", children: title }) }) }), _jsx("div", { className: "p-6 text-center", children: _jsx("p", { className: "text-black text-base md:text-lg lg:text-2xl font-bold mb-4 text-right tracking-wide ", children: description }) })] }));
+const Project = ({ title, description, en, link }) => {
+    return (_jsxs("div", { className: "flex flex-col items-center overflow-hidden  ", children: [_jsx(Link, { href: link ? link : `https://${en}.mafazaa.com`, className: "w-full", children: _jsx("div", { className: "w-full h-56 md:h-80 bg-black flex items-center justify-center rounded-lg", children: _jsx("h2", { className: "text-bright-one font-bold text-2xl md:text-3xl lg:text-5xl ", children: title }) }) }), _jsx("div", { className: "p-6 text-center", children: _jsx("p", { className: "text-black text-base md:text-lg lg:text-2xl font-bold mb-4 text-right tracking-wide ", children: description }) })] }));
 };
 export default Project;
